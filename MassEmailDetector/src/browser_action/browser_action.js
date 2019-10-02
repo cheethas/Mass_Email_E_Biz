b1a9b730c442ea5aa7e46df2e59a4935cea3ed8f
Stephen Byrne <byrnes68@tcd.ie>
2019-10-02 14:20:10 +0100

    file organisation, button visually complete, js started
    Error running js script when clicking popup, as per chrome docs can't call from here -> need to call elsewhere.

---
+++ MassEmailDetector/src/browser_action/browser_action.js
@@ -0,0 +1,37 @@
+//Browser actions JS - "Popup"
+
+//vars
+var active;
+
+document.addEventListener("DOMContentLoaded", function() {
+    restore_options();
+    console.log("options restored");
+    // event listener for the button inside popup window
+    document.getElementById('onOff').addEventListener('click', save_options);
+});
+
+// Saves options to chrome.storage
+function save_options() {
+  console.log('save options : running');
+  var onOffOption = document.getElementById("onOff").checked;
+  console.log('save options : checked var recieved');
+  chrome.storage.sync.set({
+    active: onOffOption,
+  });
+  console.log('save options : sync storage set');
+}
+
+// Restores select box and checkbox state using the preferences
+// stored in chrome.storage.
+function restore_options() {
+  chrome.storage.sync.get({
+    active: 'onOffOption',
+  }, function(items) {
+    document.getElementById('onOffOption').value = items.onOffOption;
+  });
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+  restore_options();
+  console.log('options restored');
+});
